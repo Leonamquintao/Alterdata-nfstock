18be90f2c27542b66b1c8e0742534c426373fea9
Leonam Quintão <leonamquintao@gmail.com>
2019-02-24 21:20:21 -0300

    remove some elements

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,74 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="container">
+    <div class="navmenu">
+
+      <div class="left">
+        <img class="img-logo" alt="alterdata logo" src="@/assets/logo_alterdata.svg" @click.prevent="navigate()"/>
+      </div>
+
+      <div class="right">
+        <ul>
+          <li class="nav-item">Link 1</li>
+          <li class="nav-item">Link 2</li>
+          <li class="nav-item">Link 3</li>
+        </ul>
+      </div>
+
+    </div>
   </div>
+
+  
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: "home",
+
   components: {
-    HelloWorld
-  }
+   
+  },
+  methods: {
+    navigate() {
+      window.open('https://www.alterdata.com.br/', '_blank');
+    }
+  },
 };
 </script>
+
+<style scoped>
+  .container {
+    width: 100%;
+    height: 50px;
+    padding: 20px 40px 20px 40px;
+  }
+  .navmenu {
+    background-color: '#FFF';
+    display: inline;
+  }
+  .left {
+    width: 40%;
+    float:left;
+    
+  }
+  .right {
+    width: 60%;
+    float:right
+  }
+  .img-logo {
+    width: 50%;
+    height: 50%;
+    cursor: pointer;
+  }
+  .nav-item {
+    display: inline;
+    padding: 10px;
+  }
+  .nav-item:hover {
+    background-color: #f5f5f5;
+    border-radius: 10px;
+  }
+
+</style>
+
